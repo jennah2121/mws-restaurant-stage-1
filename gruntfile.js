34da7d9fbcb65e5a8040b45fcd969c474a759943
Jennah <contactjennah@gmail.com>
2018-05-20 20:17:17 +0100

    Remove grunt-replace as API key no longer being used

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,6 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    //Configure grunt-replace
-    replace: {
-      dist: {
-        options: {
-          patterns: [
-            {
-              match: 'YOUR_API_KEY',
-              replacement: process.env.GOOGLE_MAPS_KEY
-            }
-          ]
-        },
-        files: [
-          { 
-            expand: true, 
-            flatten: true, 
-            src: ['index.html', 'restaurant.html'], 
-            dest: 'build/' 
-          }
-        ]
-      }
-    },
     //Configure grunt-contrib-copy
     copy: {
       build: {
@@ -91,9 +70,8 @@ module.exports = function (grunt) {
     }
 
   });
-  grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['replace', 'responsive_images', 'copy']);
+  grunt.registerTask('default', ['responsive_images', 'copy']);
 }
 
